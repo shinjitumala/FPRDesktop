1ebe08bc3d976b5b8b2d0bc004c3ec547bd1b53e
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-11-11 18:20:55 +0900

    Avoid cyclic dependencies.

--- src/dbg/Log.hpp
+++ src/dbg/Log.hpp
@@ -11,63 +11,13 @@
 
 #include <chrono>
 #include <dbg/Logger.hpp>
+#include <experimental/source_location>
+#include <fprd/util/ostream.hpp>
 #include <iostream>
 #include <ostream>
 
 namespace dbg {
 using namespace std;
-
-/// Use this on 'BufNewLine' to insert indent at every line
-class Indent {
-    /// String for one indent.
-    const string one_indent{"    "};
-    /// String for the current indent.
-    string indent;
-
-  public:
-    /// Increase indent by one.
-    inline void inc() { indent += one_indent; }
-    /// Decrease indent by one.
-    inline void dec() {
-        indent.erase(indent.end() - one_indent.length(), indent.end());
-    }
-
-    /// This is called to insert indent at every line.
-    /// @param buf
-    /// @return int
-    int call(streambuf &buf) {
-        return buf.sputn(indent.c_str(), indent.size());
-    }
-};
-
-/// The buffer that we use for logging.
-using LoggerBuf = LineCallbackBuf<Indent>;
-
-/// Indent tracker
-static Indent indent{};
-/// Our custom log buffer
-static LoggerBuf buf{cout, indent};
-/// Our custom log buffer for errors.
-static LoggerBuf errbuf{cerr, indent};
-/// Log stream
-static ostream out{&buf};
-/// Error log stream
-static ostream err{&errbuf};
-
-/// Indent guard. Works like a lock guard!
-struct IndentGuard {
-    /// Increases the indent.
-    IndentGuard() { indent.inc(); }
-
-    /// Decreases the indent.
-    ~IndentGuard() { indent.dec(); }
-
-    /// Should not be copyable.
-    IndentGuard(const IndentGuard &) = delete;
-
-    /// Movable
-    IndentGuard(IndentGuard &&) = default;
-};
 
 #ifndef NDEBUG
 /// Only expaned in debug builds.
@@ -80,9 +30,9 @@ struct IndentGuard {
 
 /// Fatal error
 #define fatal_error(msg)                                                       \
-    ::dbg::err << "[FATAL ERROR]["                                      \
-                      << ::std::experimental::source_location::current()       \
-                      << "] " << msg << ::std::endl;                           \
+    ::dbg::err << "[FATAL ERROR]["                                             \
+               << ::std::experimental::source_location::current() << "] "      \
+               << msg << ::std::endl;                                          \
     ::abort();                                                                 \
     ::exit(1)
 }; // namespace dbg
--- src/dbg/Logger.hpp
+++ src/dbg/Logger.hpp
@@ -90,4 +90,56 @@ template <class... Callbacks> class CombinedCallback {
             callbacks);
     }
 };
+
+/// Use this on 'BufNewLine' to insert indent at every line
+class Indent {
+    /// String for one indent.
+    const string one_indent{"    "};
+    /// String for the current indent.
+    string indent;
+
+  public:
+    /// Increase indent by one.
+    inline void inc() { indent += one_indent; }
+    /// Decrease indent by one.
+    inline void dec() {
+        indent.erase(indent.end() - one_indent.length(), indent.end());
+    }
+
+    /// This is called to insert indent at every line.
+    /// @param buf
+    /// @return int
+    int call(streambuf &buf) {
+        return buf.sputn(indent.c_str(), indent.size());
+    }
+};
+
+/// The buffer that we use for logging.
+using LoggerBuf = LineCallbackBuf<Indent>;
+
+/// Indent tracker
+static Indent indent{};
+/// Our custom log buffer
+static LoggerBuf buf{cout, indent};
+/// Our custom log buffer for errors.
+static LoggerBuf errbuf{cerr, indent};
+/// Log stream
+static ostream out{&buf};
+/// Error log stream
+static ostream err{&errbuf};
+
+/// Indent guard. Works like a lock guard!
+struct IndentGuard {
+    /// Increases the indent.
+    IndentGuard() { indent.inc(); }
+
+    /// Decreases the indent.
+    ~IndentGuard() { indent.dec(); }
+
+    /// Should not be copyable.
+    IndentGuard(const IndentGuard &) = delete;
+
+    /// Movable
+    IndentGuard(IndentGuard &&) = default;
+};
 }; // namespace dbg
