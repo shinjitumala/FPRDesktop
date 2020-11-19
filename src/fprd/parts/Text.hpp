0882c61eadc8fe43a4d40b65c01f4baeea459833
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-11-19 23:21:12 +0900

    Added changable colors.

--- src/fprd/parts/Text.hpp
+++ src/fprd/parts/Text.hpp
@@ -11,27 +11,30 @@
 #include <fprd/Window.hpp>
 
 namespace fprd {
-class Text {
+template <Source Background = Color, Source Foreground = Color> struct Text {
     Position pos;
     Size area;
     double size;
     Position text_pos;
     const Font &f;
 
-  public:
-    Text(Position pos, Size area, Margin m, const Font &f)
+    Background bg;
+    Foreground fg;
+
+    Text(Position pos, Size area, Margin m, const Font &f,
+         Background &&bg = {"000000"}, Foreground &&fg = {"ffffff"})
         : pos{pos}, area{area}, size{(area - m).h}, text_pos{[&]() {
               auto p{pos - m};
               p.y += size;
               return p;
           }()},
-          f{f} {}
+          f{f}, bg{move(bg)}, fg{move(fg)} {}
 
     void update(FPRWindow &w, string &&s) const {
-        w.set_source({"000000"});
+        w.set_source(bg);
         w.rectangle(pos, area);
         w.fill();
-        w.set_source({"ffffff"});
+        w.set_source(fg);
         w.move_to(text_pos);
         w.set_font_size(size);
         w.set_font(f);
