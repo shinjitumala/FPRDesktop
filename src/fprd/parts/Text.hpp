5b581f3a2c87792eefc9f55811934a3e09dd2d20
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-11-20 17:40:51 +0900

    Added alignment.

--- src/fprd/parts/Text.hpp
+++ src/fprd/parts/Text.hpp
@@ -9,11 +9,21 @@
 #pragma once
 
 #include <fprd/Window.hpp>
+#include <optional>
 
 namespace fprd {
-template <Source Background = Color, Source Foreground = Color> struct Text {
+enum class TextAlign : u_char {
+    center,
+    left,
+    right,
+};
+
+template <bool clear = true, TextAlign vertical = TextAlign::left,
+          Source Background = Color, Source Foreground = Color>
+struct Text {
     Position pos;
     Size area;
+    Margin m;
     double size;
     Position text_pos;
     const Font &f;
@@ -22,22 +32,36 @@ template <Source Background = Color, Source Foreground = Color> struct Text {
     Foreground fg;
 
     Text(Position pos, Size area, Margin m, const Font &f,
-         Background &&bg = {"000000"}, Foreground &&fg = {"ffffff"})
-        : pos{pos}, area{area}, size{(area - m).h}, text_pos{[&]() {
+         Foreground &&fg = {"ffffff"}, Background &&bg = {"000000"})
+        : pos{pos}, area{area}, m{m}, size{(area - m).h}, text_pos{[&]() {
               auto p{pos - m};
               p.y += size;
               return p;
           }()},
           f{f}, bg{move(bg)}, fg{move(fg)} {}
 
-    void update(FPRWindow &w, string &&s) const {
-        w.set_source(bg);
-        w.rectangle(pos, area);
-        w.fill();
-        w.set_source(fg);
-        w.move_to(text_pos);
+    void update(FPRWindow &w, string &&s) {
+        if constexpr (clear) {
+            w.set_source(bg);
+            w.rectangle(pos, area);
+            w.fill();
+        }
         w.set_font_size(size);
         w.set_font(f);
+        w.set_source(fg);
+        if constexpr (vertical == TextAlign::left) {
+            w.move_to(text_pos);
+        } else if constexpr (vertical == TextAlign::center ||
+                             vertical == TextAlign::right) {
+            const auto te{w.get_text_extent(s)};
+            auto pos{text_pos};
+            if constexpr (vertical == TextAlign::center) {
+                pos.x = pos.x + (area - m).w / 2 - te.width / 2;
+            } else if constexpr (vertical == TextAlign::right) {
+                pos.x = pos.x + (area - m).w - te.width;
+            }
+            w.move_to(pos);
+        }
         w.print_text(move(s));
     }
 };
