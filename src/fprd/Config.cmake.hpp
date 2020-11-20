8fe08817a1d58b01f5212a87c7966e417df05fe2
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-11-20 17:41:17 +0900

    Add configured file for resources.

---
+++ src/fprd/Config.cmake.hpp
@@ -0,0 +1,16 @@
+/// @file Config.cmake.hpp
+/// @author FPR (funny.pig.run __ATMARK__ gmail.com)
+///
+/// @copyright Copyright (c) 2020
+///
+/// License: Proprietary.
+/// You may not use or share this file without the permission of the author.
+
+#pragma once
+
+#include <filesystem>
+
+namespace fprd {
+using namespace ::std::filesystem;
+static inline const path resources{"@CMAKE_CURRENT_SOURCE_DIR@/res"};
+} // namespace fprd
