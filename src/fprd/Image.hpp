ad8a0ed6280df03c63e354c7f3360abc8f2e3a45
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-11-20 17:41:31 +0900

    Add masked image.

--- src/fprd/Image.hpp
+++ src/fprd/Image.hpp
@@ -11,6 +11,7 @@
 #include <cairo/cairo.h>
 #include <dbg/Log.hpp>
 #include <filesystem>
+#include <fprd/Color.hpp>
 
 namespace fprd {
 using namespace std;
@@ -24,13 +25,25 @@ class Image {
     cairo_surface_t *i;
 
   public:
+    int w;
+    int h;
+
     Image(path &&path)
         : i{[&]() {
               if (!exists(path)) {
                   fatal_error("Missing file: " << path);
               }
               return cairo_image_surface_create_from_png(path.c_str());
-          }()} {}
+          }()},
+          w{cairo_image_surface_get_width(i)}, h{cairo_image_surface_get_height(
+                                                   i)} {}
+    Image(path &&path, Color mask) : Image{move(path)} {
+        auto *const c{cairo_create(i)};
+        cairo_set_operator(c, CAIRO_OPERATOR_ATOP);
+        cairo_set_source_rgba(c, mask.r, mask.g, mask.b, mask.a);
+        cairo_paint(c);
+        cairo_destroy(c);
+    }
 
     ~Image() { cairo_surface_destroy(i); }
 
