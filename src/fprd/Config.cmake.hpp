453a85953ee20c649e7cf160f3632777d0cf8558
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-12-15 18:23:54 +0900

    Better config.

--- src/fprd/Config.cmake.hpp
+++ src/fprd/Config.cmake.hpp
@@ -12,5 +12,11 @@
 
 namespace fprd {
 using namespace ::std::filesystem;
+using namespace ::std::chrono;
+
+/// Location for resources
 static inline const path resources{"@CMAKE_CURRENT_SOURCE_DIR@/res"};
+static inline const auto data_update_interval{1s};  // Update data every second
+static inline const auto fps{60};                   // Frames per second
+static inline const auto draw_interval{duration_cast<microseconds>(1s) / fps};
 }  // namespace fprd
