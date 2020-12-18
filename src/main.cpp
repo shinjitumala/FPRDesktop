05a1f066c94e262913a6a58fa12585680e922595
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-12-18 17:10:59 +0900

    GPU monitor works now.

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 /// License: Proprietary.
 /// You may not use or share this file without the permission of the author.
 
+#include <GPU.hpp>
 #include <chrono>
 #include <csignal>
 #include <dbg/Log.hpp>
-#include <fprd/widgets/CPU.hpp>
-#include <fprd/widgets/Nvidia.hpp>
+#include <fprd/Threads.hpp>
 #include <thread>
 
 namespace fprd {
@@ -19,82 +19,12 @@ using namespace ::std::chrono;
 
 /// Set to false when exiting.
 atomic<bool> run{true};
-/// Draw interval.
-constexpr auto interval{16ms};
-
-constexpr auto draw_loop{[&run = run](auto f) {
-    while (run) {
-        const auto tp{now()};
-        f();
-        this_thread::sleep_until(tp + interval);
-    }
-}};
 
 /// Signal handler
 /// @param signal
 /// @return auto
 auto stop(int signal) { run = false; }
 
-/// The window with GPU stats
-class GPUWindow {
-    nvml::NVML nvml{};
-    vector<nvml::Device> devs;
-
-    FPRWindow w;
-
-    vector<widget::Nvidia> gpus;
-
-    /// Drawing thread.
-    thread t;
-
-   public:
-    GPUWindow()
-        : nvml{},
-          devs{nvml.get_devices(run)},
-          w{":0.0", {0, 0}, widget::Nvidia::size.scale({devs.size(), 1})},
-          gpus{[&]() {
-              vector<widget::Nvidia> gpus;
-              for (auto i{0U}; i < devs.size(); i++) {
-                  gpus.emplace_back(widget::Nvidia{
-                      w, devs[i], {i * widget::Nvidia::size.w, 0}});
-              }
-              return gpus;
-          }()},
-          t{draw_loop, [&]() { draw(); }} {}
-
-    GPUWindow(const GPUWindow &) = delete;
-    ~GPUWindow() { t.join(); }
-
-   private:
-    void draw() {
-        for_each(gpus.begin(), gpus.end(),
-                 [this](auto &widget) { widget.draw(w); });
-        w.flush();
-    }
-};
-
-/// The window with CPU stats
-struct CPUWindow {
-    query::CPU q;
-    FPRWindow win;
-    widget::CPU widget;
-
-    /// Drawing thread.
-    thread t;
-
-    CPUWindow()
-        : q{run},
-          win{":0.0", widget::CPU::size.top_right({1920, 0}),
-              widget::CPU::size},
-          widget{win, q, {0, 0}},
-          t{draw_loop, [&]() { draw(); }} {}
-    ~CPUWindow() { t.join(); }
-
-    void draw() {
-        widget.draw(win);
-        win.flush();
-    }
-};
 }  // namespace fprd
 
 int main(int argc, char **argv) {
@@ -102,8 +32,8 @@ int main(int argc, char **argv) {
     std::signal(SIGINT, stop);
     std::signal(SIGKILL, stop);
 
-    GPUWindow gpus{};
-    CPUWindow cpu{};
+    GPUWindow gpus{run, {0, 0}};
+    // CPUWindow cpu{};
 
     return 0;
 }
