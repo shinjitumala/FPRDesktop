bf53ef6014771c9467e1cda94bc3178ccb9d2357
shinjitumala <stumala2797@dune.ocn.ne.jp>
2020-12-25 15:15:27 +0900

    Documentation. Removed unused operator.

--- src/fprd/draw/animated/ABar.hpp
+++ src/fprd/draw/animated/ABar.hpp
@@ -13,6 +13,12 @@
 
 namespace fprd {
 
+/// Animated version of Bar.
+/// @tparam o
+/// @tparam d
+/// @tparam Frame
+/// @tparam Empty
+/// @tparam Filled
 template <Orientation o, Direction d, cairo::source Frame = Color,
           cairo::source Empty = Color, cairo::source Filled = Color>
 class AnimatedBar : public Bar<o, d, Frame, Empty, Filled> {
@@ -23,22 +29,34 @@ class AnimatedBar : public Bar<o, d, Frame, Empty, Filled> {
     float increment{0};  // The increment per draw call.
 
    public:
+    /// Default constructor.
     AnimatedBar() = default;
+    /// Copying is not allowed.
     AnimatedBar(const AnimatedBar&) = delete;
+    /// Moving is allowed, however.
     AnimatedBar(AnimatedBar&&) noexcept = default;
+    /// Initialzation from a Bar.
+    /// @param b
     AnimatedBar(Base b) : Base{b} {};
 
+    /// Update the target percentage.
+    /// Currently hard-coded such that this function must be called every second
+    /// or it breaks.
+    /// TODO: Make the update interval adjustable via template parameters.
+    /// @param target_percentage
     void update(float target_percentage) {
         increment = (target_percentage - current) / fps;
     }
 
+    /// Call this every frame.
+    /// @param w 
     void draw(Window& w) {
         Base::draw(w, current);
         current += increment;
     }
 
+    /// In case you need to peek the current value of the bar.
+    /// @return float 
     [[nodiscard]] float current_percentage() const { return current; }
-
-    auto operator->() { return this; };
 };
 };  // namespace fprd
