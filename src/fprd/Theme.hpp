35ddad8cb76ec49fba70517f38dfc689cf98a454
shinjihoshino <stumala2797@dune.ocn.ne.jp>
2021-06-30 16:26:40 +0900

    Delete unused.

--- src/fprd/Theme.hpp
+++ src/fprd/Theme.hpp
@@ -17,26 +17,9 @@ using namespace std;
 /// Some preset colors and stuff for some theming.
 namespace theme {
 constexpr Color black{"000000"};
-constexpr Color grey{"7d818d"};
 constexpr Color white{"ffffff"};
-constexpr Color green{"06ae7a"};
-constexpr Color red{"c70700"};
-constexpr Color blue{"68c8f2"};
-constexpr Color yellow{"fef4ad"};
 
 /// Some preset fonts.
 const cairo::Font normal{"Noto Sans Mono", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL};
-const cairo::Font bold{"Noto Sans Mono", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD};
-
-/// Some preset sizes.
-constexpr auto large_h{20};
-constexpr auto medium_h{12};
-constexpr auto small_h{8};
-constexpr Area<float> large_area(float w) { return {w, large_h}; };
-constexpr Area<float> medium_area(float w) { return {w, medium_h}; };
-constexpr Area<float> small_area(float w) { return {w, small_h}; };
-
-/// Compute interval based on Config
-template <number I> auto get_interval(I target, I current) { return (target - current) / fps; }
 }; // namespace theme
 }; // namespace fprd
