d1f315e94e96a427d4b47de72c3455f6576a5860
shinjihoshino <stumala2797@dune.ocn.ne.jp>
2021-06-30 16:28:03 +0900

    Experimental run.

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,44 @@
 /// License: Proprietary.
 /// You may not use or share this file without the permission of the author.
 
-#include <CPU.hpp>
-#include <GPU.hpp>
-#include <System.hpp>
+// #include <GPU.hpp>
+// #include <System.hpp>
 #include <chrono>
 #include <csignal>
 #include <dbg/Log.hpp>
-#include <fprd/Threads.hpp>
+#include <fprd/probes/CPU.hpp>
+#include <fprd/probes/NVML.hpp>
 #include <thread>
+
+#include <CPU.hpp>
 
 namespace fprd {
 using namespace ::std;
 using namespace ::std::chrono;
 
-/// Set to false when exiting.
-atomic<bool> run{true};
+atomic<CPUWindow *> cpu{nullptr};
 
 /// Signal handler
 /// @param signal
 /// @return auto
-auto stop(int signal) { run = false; }
+auto stop(int signal) -> void {
+    if (cpu != nullptr) {
+        cpu.load()->stop();
+    }
+};
 
 } // namespace fprd
 
-int main(int argc, char **argv) {
+auto main() -> int {
     using namespace ::fprd;
     std::signal(SIGINT, stop);
     std::signal(SIGKILL, stop);
+
+    // Since we do not use C, we can disable this to be faster.
+    ios::sync_with_stdio(false);
+
+    CPUWindow w{{0, 0}};
+    fprd::cpu = &w;
 
     return 0;
 }
