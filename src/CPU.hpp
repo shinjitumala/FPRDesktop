05708fee8cc0fd2586f3b8f118eff814641cc7c9
shinjihoshino <stumala2797@dune.ocn.ne.jp>
2021-07-02 22:24:44 +0900

    Added diplaying of processes sorted by cpu usage.

--- src/CPU.hpp
+++ src/CPU.hpp
@@ -15,12 +15,15 @@
 #include <fprd/probes/CPU.hpp>
 #include <fprd/probes/NVML.hpp>
 #include <fprd/util/ranges.hpp>
+#include <llvm/ADT/SmallString.h>
 #include <thread>
 
 namespace fprd {
 using namespace std;
 
 namespace cpu {
+constexpr auto max_procs{32};
+
 /// Storage type for data that requires storing the previous value to present.
 struct PrevDynamicInfo {
     probe::cpu::Stat stat;
@@ -34,18 +37,36 @@ template <class Window> struct TDrawState {
     element::HGraph<element::graph_standard, Window> mem_hist;
 };
 
+struct Proc {
+    pid_t pid;
+    char mode;
+    mutable llvm::SmallString<32> name;
+    long usage;
+    unsigned long mem; // KB
+
+    Proc() = default;
+
+    Proc(const probe::cpu::Proc &p, long usage) : pid{p.pid}, mode{p.state}, usage{usage}, mem{p.mem} {
+        get_name(pid, name);
+    }
+
+    auto operator<(const Proc &o) const -> auto { return usage > o.usage; }
+};
+
 /// Information that have to be sampled from the DynamicInformation.
 struct SampledInfo {
+    unsigned long cpu_time;                         // Total cycles for all threads.
     long double util;                               // Ratio [0, 1]
     llvm::SmallVector<long double, 16> thread_util; // Ratio [0, 1]
     long double mem;                                // Ratio [0, 1]
     long double mem_used;                           // GB
+    llvm::SmallVector<Proc, 512> procs;
 };
 
 class Widget {
   public:
     static constexpr int width{64};
-    static constexpr int height{36};
+    static constexpr int height{33 + max_procs};
 
     static constexpr int left_end{21};
 
@@ -95,7 +116,7 @@ class Widget {
 
                     return bars;
                 }(),
-            .mem_hist = {buf, {0, 17}, {width, 27}},
+            .mem_hist = {buf, {0, 18}, {width, 28}},
         };
     };
 
@@ -108,10 +129,10 @@ class Widget {
         const auto namesize{ci.name.size()};
         const auto lmargin{(width - namesize) / 2};
         buf.printf(0, "%*c%s", lmargin, ' ', ci.name.data());
-
         buf.printf(1, "Utilization:        ");
-        buf.printf(16, "Memory:        / %3.0LfGB", ci.mem_total * 1e-6L);
-        buf.printf(27, "Temp:     ℃");
+        buf.printf(17, "Memory:         / %3.0LfGB", ci.mem_total * 1e-6L);
+        buf.printf(29, "Temp:     ℃");
+        buf.printf(31, "Processes");
     }
 
   public:
@@ -142,7 +163,7 @@ class Widget {
 
             w.update(buf);
             probe::cpu::update_data(cur, thread_count);
-            update_state_info(info, cur, prev, sampled);
+            update_sample(info, cur, prev, sampled);
             update_buf(sampled, cur, draw, buf);
             update_prev(cur, prev);
 
@@ -167,10 +188,19 @@ class Widget {
 
         // Memory
         draw.mem_hist.update(buf, si.mem);
-        buf.printf_st(16, 8, 15, "%6.2Lf", si.mem_used);
+        buf.printf_st(17, 8, 16, "%7.3Lf", si.mem_used);
 
-        // di.temp
-        buf.printf_st(27, 6, 10, "%3d", di.temp);
+        // Temperature
+        buf.printf_st(29, 6, 10, "%3d", di.temp);
+
+        // Processes
+        for (auto i{0}; i < max_procs; i++) {
+            buf.printf(32 + i, "%*c", width, ' ');
+        }
+        for (auto [idx, p] : si.procs | enumerate) {
+            buf.printf(32 + idx, "%-6d %c %32.32s %6.2Lf%% %12.0LfMB", p.pid, p.mode, p.name.c_str(),
+                       (long double)p.usage / si.cpu_time * 100.0L, p.mem * 1e-6L);
+        }
     }
 
     /// Called every frame to update the SampledInfo, which requires processing the DynamicInfo from the previous
@@ -179,23 +209,49 @@ class Widget {
     /// @param di
     /// @param prev
     /// @param state
-    static auto update_state_info(const ConstInfo &ci, const DynamicInfo &cur, const PrevDynamicInfo &prev,
-                                  SampledInfo &sampled) -> void {
+    static auto update_sample(const ConstInfo &ci, const DynamicInfo &cur, const PrevDynamicInfo &prev,
+                              SampledInfo &sampled) -> void {
         static auto get_util{[](const probe::cpu::Stat::Line &prev, const probe::cpu::Stat::Line &cur) {
             const auto idle{cur.idle - prev.idle};
             const auto active{(cur.user - prev.user) + (cur.user - prev.user)};
 
-            return (long double)active / (active + idle);
+            return make_pair((long double)active / (active + idle), active + idle);
         }};
-        sampled.util = get_util(prev.stat.cpu, cur.stat.cpu);
+        sampled.util = get_util(prev.stat.cpu, cur.stat.cpu).first;
         tie(sampled.mem, sampled.mem_used) = [&] {
             const auto used_kb{ci.mem_total - cur.mem.free}; // KB
             const auto used_gb{used_kb * 1e-6L};             // GB
             const auto used_ratio{(long double)used_kb / ci.mem_total};
             return make_pair(used_ratio, used_gb);
         }();
+        sampled.cpu_time = 0;
         for (auto [p, c, r] : fprd::zip(prev.stat.threads, cur.stat.threads, sampled.thread_util)) {
-            r = get_util(p, c);
+            unsigned long u;
+            tie(r, u) = get_util(p, c);
+            sampled.cpu_time += u;
+        }
+
+        // Processes
+        sampled.procs.clear();
+        for (const auto &p : prev.procs) {
+            const auto *const itr{find_if(cur.procs, [&p](auto &c) { return p.pid == c.pid; })};
+            if (itr == cur.procs.end()) {
+                // Ignore if 'p' did not exist previously.
+                continue;
+            }
+            const auto &c{*itr};
+            const auto usage{c.usage - p.usage};
+            if (usage <= 0) {
+                // Ignore if usage is below 0.
+                continue;
+            }
+
+            sampled.procs.emplace_back(c, usage);
+        }
+        sort(sampled.procs.begin(), sampled.procs.end());
+        const auto procs_count{sampled.procs.size()};
+        if (max_procs < procs_count) {
+            sampled.procs.resize(max_procs);
         }
     }
 
